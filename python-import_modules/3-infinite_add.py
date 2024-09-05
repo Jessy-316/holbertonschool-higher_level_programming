4a214f80522af9cf1e883224680100626139f298
franciscobaez <jessiebaez@outlook.com>
2024-09-05 12:55:11 -0400

    task 4 attempt 2

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     total = 0
     for index in range(1, len(sys.argv)):
         total = total + int(sys.argv[index])
-        print("{}".format(total))
+    print("{}".format(total))
