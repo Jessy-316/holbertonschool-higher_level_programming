b98871948a18fb00626a572a912910089193f0ab
franciscobaez <jessiebaez@outlook.com>
2024-09-16 13:37:05 -0400

    task 3 attempt 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,10 +4,11 @@
 
 class Square:
     """Square class with private class attribute"""
-    def __init__(self, size = 0):
+
+    def __init__(self, size=0):
         """Initialize a square with size validation!"""
         if not isinstance(size, int):
-            raise TypeError ("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
