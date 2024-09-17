d143965ff5340981eb5c749a62b9243e7975388f
franciscobaez <jessiebaez@outlook.com>
2024-09-17 09:55:56 -0400

    task 7 attempt 2

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -101,18 +101,3 @@ class Square:
         for index in range(self.__size):
             print("_" * self.__position[0], end="")
             print("#" * self.__size)
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
