d2acb84f325b5b491e0f74386b0c0b71b67aca43
franciscobaez <jessiebaez@outlook.com>
2024-09-24 13:28:37 -0400

    task 11 attempt 2

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -17,7 +17,7 @@ class Square(Rectangle):
     def area(self):
         """Returns the area of the square."""
         return super().area()
-    
+
     def __str__(self):
         """Returns a string representation of the square."""
         return (f"[Square] {self.__size}/{self.__size}")
