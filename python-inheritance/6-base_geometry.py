7480fa47e4682f3e70d467562b5d45bb9928ab51
franciscobaez <jessiebaez@outlook.com>
2024-09-24 11:57:30 -0400

    task 7 attempt 1

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""BaseGeometry class module"""
+
+
+class BaseGeometry:
+    """Creates an empty class called BaseGeometry."""
+
+    def area(self):
+        """Raises an error indicating the area method is not implemented"""
+        raise Exception("area() is not implemented")
