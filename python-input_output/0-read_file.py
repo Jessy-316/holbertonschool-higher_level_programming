83f41cb46a17a0037b83baab4f72c6221442eee5
franciscobaez <jessiebaez@outlook.com>
2024-09-30 12:53:22 -0400

    task 1 attempt 2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Python executable file."""
 
+
 def read_file(filename=""):
     """Function that readsa text file and prints it to stdout."""
     with open(filename, 'r', encoding="utf-8") as file:
