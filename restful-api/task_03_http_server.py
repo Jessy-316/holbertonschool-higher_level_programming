c0698db567fd9c86fdb3cef030190f1eacb744ce
franciscobaez <jessiebaez@outlook.com>
2024-10-16 13:01:36 -0400

    task 4 attempt 2

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -54,5 +54,6 @@ def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
+
 if __name__ == '__main__':
     run(handler_class=MyHandler)
