23273c60ae0babb5684110df37f2893e746d8762
franciscobaez <jessiebaez@outlook.com>
2024-10-22 14:35:12 -0400

    task 100 attempt 4

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -15,5 +15,4 @@ COLLATE utf8mb4_unicode_ci;
 -- Step 3: Convert the field name in first_table to utf8mb4
 ALTER TABLE first_table
 MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
