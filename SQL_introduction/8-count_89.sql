0011d65c449665a85a0ced9aaaee7b8eb5657954
franciscobaez <jessiebaez@outlook.com>
2024-10-22 11:27:51 -0400

    task 8 attempt 3

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,6 +1,4 @@
 -- Write a script that displays the number of records
---with id = 89 in the table first_table of the database hbtn_0c_0
+-- with id = 89 in the table first_table of the database hbtn_0c_0
 
-SELECT COUNT(id)
-FROM first_table
-WHERE id = 89;
+SELECT COUNT(id) FROM first_table WHERE id = 89;
