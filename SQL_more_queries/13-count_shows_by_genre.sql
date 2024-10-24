fc4f68b272a6613a8a3bd6d2b344078849785c6c
franciscobaez <jessiebaez@outlook.com>
2024-10-24 10:45:31 -0400

    task 13 attempt 5

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -11,6 +11,6 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
-ON tv_genres.id = tv_genres.id
+ON tv_show_genres.genre.id = tv_genres.id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
