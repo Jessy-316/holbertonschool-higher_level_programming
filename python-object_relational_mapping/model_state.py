b235dd2cea9caac3109f694ba4d280d3a46a06ac
franciscobaez <jessiebaez@outlook.com>
2024-10-31 14:14:28 -0400

    task 6 attempt 1

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""First state model"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Base class for the states table"""
+    __tablename__ = "states"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column("name", String(128), nullable=False)
+    cities = relationship("city", back_populates="state")
